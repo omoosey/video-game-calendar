055e747a06d20f4102c89b66cd561284874dcfaa
Mariano Salamanca <msalamancajr@gmail.com>
2018-11-20 13:30:11 -0500

    Add Months Array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    games: [],
+    months: [{name: 'January', numOfDays: 31}, {name: 'February', numOfDays: 28}, {name: 'March', numOfDays: 31}, 
+              {name: 'April', numOfDays: 30}, {name: 'May', numOfDays: 31}, {name: 'June', numOfDays: 30}, 
+              {name: 'July', numOfDays: 31}, {name: 'August', numOfDays: 31}, {name: 'September', numOfDays: 30}, 
+              {name: 'October', numOfDays: 31}, {name: 'November', numOfDays: 30}, {name: 'December', numOfDays: 31}, ]
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <h1>Game Release Calendar</h1>
           <div id="calendar">
             <div className="month">
+              <h2>{this.state.months[0].name}</h2>
               <div className="week">
                 <div className="day">
                 </div>
