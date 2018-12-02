05cc9760e7e9ec20740e819e87e26d75067961c3
Mariano Salamanca Jr <msalamancajr@gmail.com>
2018-12-01 20:32:09 -0500

    fetch request to release_dates

--- src/App.js
+++ src/App.js
@@ -10,7 +10,31 @@ class App extends Component {
               {name: 'October', numOfDays: 31}, {name: 'November', numOfDays: 30}, {name: 'December', numOfDays: 31}, ]
   }
 
+  componentWillMount = () => {
+    fetch('https://api-endpoint.igdb.com/release_dates/154513', {
+      mode: 'cors',
+      headers: {
+        'user-key': 'fe6b183a901554b5418e7b3a22ca8288',
+        'Accept': 'application/json'
+      } 
+    }).then(res => res.json())
+    .then(data => console.log(data))
+  }
+
   render() {
+    let days = [];
+
+    /* (day + Math.floor(2.6month - 0.2) - 2century + year + Math.floor(year/4) + Math.floor(century/4))%7 
+    ** day (1 to 31)
+    ** month (1 = March, ..., 10 = December, 11 = Jan, 12 = Feb) Treat Jan & Feb as months of the preceding year
+    ** century (1987 has C = 19)
+    ** year (1987 has Y = 87 except Y = 86 for Jan & Feb)
+    ** date (0 = Sunday, ..., 6 = Saturday)
+    */
+    let date = (1 + Math.floor((2.6*11) - 0.2) - (2*20) + 18 + Math.floor(18/4) + Math.floor(20/4))%7;
+
+    console.log(date);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -18,6 +42,30 @@ class App extends Component {
           <div id="calendar">
             <div className="month">
               <h2>{this.state.months[0].name}</h2>
+              <div className="week">
+                <div className="day">
+                </div>
+                <div className="day">
+                </div>
+                <div className="day">
+                </div>
+                <div className="day">
+                </div>
+                <div className="day">
+                </div>
+                <div className="day">
+                </div>
+                <div className="day">
+                </div>
+              </div>
+              <div className="week">
+                <div className="day">
+                </div>
+              </div>
+              <div className="week">
+                <div className="day">
+                </div>
+              </div>
               <div className="week">
                 <div className="day">
                 </div>
